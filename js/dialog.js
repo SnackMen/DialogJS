cf631939b56620c7bf518fb3e35657109fbc3aec
Kanon <vipkia@sina.cn>
2016-07-05 11:17:31 +0800

    DialogJS1.0正式上线

--- js/dialog.js
+++ js/dialog.js
@@ -3,13 +3,12 @@
  */
 jsPlumb.ready(function() {
 
-
     jsPlumb.importDefaults({
         DragOptions:{cursor:'pointer'},//移动到点上显示手指
         PaintStyle:{strokeStyle:'#666'},//元素默认颜色
         EndpointStyle:{radius:11,strokeStyle:"#5bc0de"},//连接点颜色
         Endpoint:"Dot",//连接点形状
-        Anchors:[["TopCenter"],["BottomCenter"],["LeftCenter"],["RightCenter"]]//连接点位置
+        Anchors:["TopCenter"]//连接点位置
     });
 
     //可连接点自动匹配，给出提示
@@ -27,42 +26,18 @@ jsPlumb.ready(function() {
         isSource:true,//是否可以拖动
         scope:"dot",//匹配类型连接点可以连接
         connectorStyle:{strokeStyle:color1,lineWidth:6},
-        connector:"Flowchart",
+        connector:"Bezier",
         maxConnections:4,
         isTarget:true,
         dropOptions:DropOptions
     };
     var idIndex=0;
     //拖动
-    //jsPlumb.draggable($(".item"),{
-    //    //appendTo:"#show-dialog",
-    //    helper:"clone",
-    //    scope:"flag"
-    //});
+    //直接孩子结点
    $("#left-title").children().draggable({
-       //appendTo:"#show-dialog",
        helper:"clone",
        scope:"flag"
    });
-   // jsPlumb.droppable($("#show-dialog"),{
-   //     activeClass:"ui-state-default",
-   //     hoverClass:"ui-state-hover",
-   //     scope:"flag",
-   //     accept:":not(ui-sortable-helper)",
-   //     drop:function(event,ui){
-   //         idIndex=idIndex+1;
-   //         var left=parseInt(ui.offset.left-$(this).offset().left);
-   //         var top=parseInt(ui.offset.top-$(this).offset().top);
-   //         var name=ui.draggable[0].id;
-   //         var cla=ui.draggable[0].className;
-   //         var trueId=name+"-"+idIndex;
-   //         $(this).append("<div class=\""+cla+"\" id=\""+trueId+"\"></div>");
-   //         $("#"+trueId).css("left",left).css("top",top).css("position","absolute").css("margin","0px");
-   //         $(".item").draggable({
-   //             containment:"#show-dialog"
-   //         });
-   //     }
-   // });
     //放置
         $("#show-dialog").droppable({
         //hoverClass:"dropHover",
@@ -82,26 +57,8 @@ jsPlumb.ready(function() {
             jsPlumb.addEndpoint(trueId,{anchors:"RightMiddle"},DropExampleOptionOne);
             jsPlumb.addEndpoint(trueId,{anchors:"TopCenter"},DropExampleOptionOne);
             jsPlumb.addEndpoint(trueId,{anchors:"BottomCenter"},DropExampleOptionOne);
-            jsPlumb.addEndpoint(trueId,DropExampleOptionOne);
-            jsPlumb.draggable($("#"+trueId));
-            $("#"+trueId).draggable({
-                containment:"parent"
-            });
-            //jsPlumb.draggable($("#"+trueId),{
-            //    containment:"#show-dialog"
-            //});
-            //克隆标签 拖动
-
-
-            //jsPlumb.draggable($(".item"));
-            //jsPlumb.addEndpoint(trueId,{anchor:"TopCenter"},DropExampleOptionOne);
-            //jsPlumb.addEndpoint(trueId,{anchor:"LeftCenter"},DropExampleOptionOne);
-            //jsPlumb.addEndpoint(trueId,{anchor:"RightCenter"},DropExampleOptionOne);
+            jsPlumb.draggable(trueId);
         }
     });
-
-    //拖动设置
-
-
 
 });
